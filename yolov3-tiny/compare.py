b358236752af30228b286ad81df560840146906e
Rui-Tao Dong <ruitao.dong@gmail.com>
2022-03-09 08:39:12 -0800

    added -dmor.json

--- yolov3-tiny/compare.py
+++ yolov3-tiny/compare.py
@@ -21,14 +21,14 @@ for t in decoded:
     if not t_base in dmor_dict:
         continue
 
-    prop = dmor_dict[t_base]
+    prop = dmor_dict.pop(t_base)
     if not 'QR' == prop['type']:
         continue
 
     with Image.open(t_png) as im:
         width, height = im.size
 
-    properties = {'score':'float:0.3'}
+    properties = {'score':'float'}
 
     t_json = t.replace('.txt', '-yolov3-tiny.json')
     #print(t_json)
@@ -44,3 +44,24 @@ for t in decoded:
                                   [[(x-w/2,-y+h/2), (x-w/2, -y-h/2), (x+w/2, -y-h/2), (x+w/2, -y+h/2), (x-w/2,-y+h/2)]]},
                      'type': 'feature',
                      'properties': {'score': s}})
+
+    t_dmor = t.replace('.txt', '-dmor.json')
+
+    corners = prop.pop('corners')
+    x0 = corners['x0']
+    x1 = corners['x1']
+    x2 = corners['x2']
+    x3 = corners['x3']
+    y0 = corners['y0']
+    y1 = corners['y1']
+    y2 = corners['y2']
+    y3 = corners['y3']
+
+    dmor_properties = {'type':'str', 'rows': 'int', 'cols':'int', 'polarity':'int'}
+    with fiona.open(t_dmor, 'w', driver='GeoJSON',
+                    schema = {'geometry': 'Polygon', 'properties': dmor_properties}) as dmor:
+        dmor.write({'geometry': {'type': 'Polygon', 'coordinates':
+                                  [[(x0,-y0),(x1,-y1),(x2,-y2),(x3,-y3),(x0,-y0)]]},
+                     'type': 'feature',
+                     'properties': prop})
+        
